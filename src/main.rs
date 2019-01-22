3763e97db11b01f46168b078a57e557e45463f35
Joseph Gerardot <indyJAG@gmail.com>
2019-01-22 01:44:23 -0500

    Add outline comments to main

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+  // Ingest Dancer data
+
+  // Try to give every dancer a request
+
+  // Give out all possible requests
+
+  // Satisfy dancer minimums
+
+  // Satisfy dance minimums
+
+  // Dump results to output
+}
